fbf23b3d75d9b6af64ebeb5e3417c9dce505a6c6
Philippe-78 <allotaubus@gmail.com>
2024-11-08 21:04:01 +0100

    amélioration front : centrage de la navbar

--- Front/src/components/navBar/NavBar.tsx
+++ Front/src/components/navBar/NavBar.tsx
@@ -1,15 +1,17 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
-    <>
+    <nav className="navbar">
       <button>
         <Link to="/">LoginPage</Link>
       </button>
       <button>
         <Link to="/firstpage">WorkingPage</Link>
       </button>
-    </>
+    </nav>
   );
 };
 
